cd017cf28455d368a779245b3e529bbc13e69ec0
ya0201 <you@example.com>
2020-10-20 23:03:56 +0900

    Initial commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+variable instances {}
+
+locals {
+# ipアドレスのlist
+  ips = flatten([for machines_in_az in values(var.instances): [for machine_map in machines_in_az: keys(machine_map)]])
+
+# ipアドレスとazのペアのlist
+  azs = transpose({for az, machines_in_az in var.instances: az => flatten([for machine_map in machines_in_az: keys(machine_map)])})
+
+# instanceの個数(ipアドレスの個数)
+  count = length(flatten(values(var.instances)))
+
+# ipとhostnameの組み合わせのリストっぽいもの
+  names = flatten(values(var.instances))
+}
+
+output "ips" {
+  value = local.ips
+}
+
+output "azs" {
+  value = local.azs
+}
+
+# output "azs2" {
+#   value = local.azs2
+# }
+
+output "count" {
+  value = local.count
+}
+
+output "names" {
+  value = local.names
+}
